41d5eb715a354aa3e6e8f269834950087343ce5a
lecareli <leticia.careli@outlook.com>
2024-06-25 10:45:47 -0300

    :sparkles: feat: list roles

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Spatie\Permission\Models\Role;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('admin.index');
+        return view('admin.index', [
+            'roles' => Role::with('permissions')->where('name', '!=', 'super-admin')->get(),
+        ]);
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,16 +4,25 @@
     <h1>Admin</h1>
 
     <div class="row">
-
         <div class="col-9">
             <h5>Roles</h5>
-        </div>
 
+            <ul class="list-group">
+                @forelse ($roles as $roles)
+                    <li class="list-group-item">{{ $role->name }}</li>
+                @empty
+                    <li class="list-group-item">No roles created, yet</li>
+                @endforelse
+            </ul>
+
+        </div>
         <div class="col-3">
             <h5>Permissions</h5>
         </div>
-
     </div>
+
+    <hr>
+
     <div class="row">
         <div class="col-12">
             <h5>Users</h5>
