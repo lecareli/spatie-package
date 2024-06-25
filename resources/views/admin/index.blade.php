2091230877f29483d6ead38aca27db34ac37c239
lecareli <leticia.careli@outlook.com>
2024-06-25 10:54:56 -0300

    :sparkles: feat: add list permissions of roles

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,19 +4,29 @@
     <h1>Admin</h1>
 
     <div class="row">
-        <div class="col-9">
+        <div class="col-3">
             <h5>Roles</h5>
 
             <ul class="list-group">
                 @forelse ($roles as $role)
-                    <li class="list-group-item">{{ Str::ucfirst($role->name) }}</li>
+                    <li class="list-group-item">
+                        {{ Str::ucfirst($role->name) }}
+
+                        <ul class="list-group">
+                            @forelse ($role->permissions as $permission)
+                                <li class="list-group-item"> {{ Str::ucfirst($permission->name) }}</li>
+                            @empty
+                                <li class="list-group-item">No permissions to this role</li>
+                            @endforelse
+                        </ul>
+                    </li>
                 @empty
                     <li class="list-group-item">No roles created, yet</li>
                 @endforelse
             </ul>
 
         </div>
-        <div class="col-3">
+        <div class="col-9">
             <h5>Permissions</h5>
         </div>
     </div>
